ea4098d123823d84fb64ff8a0d82e78df0a8c2ad
annk1995 <kabuikariuki052@gmail.com>
2022-06-22 21:20:52 +0300

    added changes

--- tubonge/urls.py
+++ tubonge/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('navbar', views.nav, name='nav'),
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('about',views.about,name='about'),
      path('footer',views.footer,name='footer'),
     
